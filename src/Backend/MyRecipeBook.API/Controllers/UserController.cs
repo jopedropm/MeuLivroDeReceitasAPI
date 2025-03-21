27595723ac0d80a6736999f6879e3d190c391dce
João Pedro <jpjopedro7@gmail.com>
2025-03-21 13:02:26 -0300

    initial commit

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Requests.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Communication.Requests.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
