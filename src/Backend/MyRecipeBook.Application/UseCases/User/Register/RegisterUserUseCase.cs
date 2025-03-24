e1b6947f09b9c0247480a825f81073453817ed57
João Pedro <jpjopedro7@gmail.com>
2025-03-24 12:50:54 -0300

    Feat: adding usecase to register an user

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,30 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisterUserJson Execute(RequestRegisterUserJson request)
+        {
+            ValidateUser(request);
+
+            return new ResponseRegisterUserJson
+            {
+                Name = request.Name
+            };
+        }
+
+        private void ValidateUser(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
