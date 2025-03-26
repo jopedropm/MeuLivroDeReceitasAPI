b20a2b64849b24a969ea28002cb420cae4102423
João Pedro <jpjopedro7@gmail.com>
2025-03-26 12:54:39 -0300

    Feat: adding culture middleware

---
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace MyRecipeBook.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault().Split(',')[0];
+
+        var cultureInfo = new CultureInfo(requestedCulture);
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
