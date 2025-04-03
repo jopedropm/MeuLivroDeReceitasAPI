f3d01be0cee7070bf4847082df93ac8df8b6141d
João Pedro <jpjopedro7@gmail.com>
2025-04-03 12:54:44 -0300

    Feat: DBContext

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+public class UserRepository
+{
+    private readonly MyRecipeBookDBContext _dbContext;
+
+    public UserRepository(MyRecipeBookDBContext dbContext) => _dbContext = dbContext;
+
+
+}
